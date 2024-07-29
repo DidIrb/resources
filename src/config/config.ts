32dfbefb8d2c80e16b1c85495a532cc7d8e89271
DidIrb <118119916+DidIrb@users.noreply.github.com>
2024-07-29 07:02:23 +0000

    Built test file: (2bcc187) Build branch creation

--- src/config/config.ts
+++
@@ -1,5 +0,0 @@
-const url: string = import.meta.env.VITE_BACKEND_URL;
-const loginRoute = import.meta.env.VITE_ADMIN_LOGIN_URL
-
-export default {url, loginRoute}
-
--- src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-import { ResourceList } from "./common/list";
-
-export const Home = () => {
-  return (
-    <div className="px-4">
-      <ResourceList />
-    </div>
-  )
-};
